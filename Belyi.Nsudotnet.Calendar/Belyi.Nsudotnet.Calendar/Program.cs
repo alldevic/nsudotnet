ceeab5ec1e2b4e2648f5fd73a7a478fad4e2076e
Nikolay Bely <belyynikolay@gmail.com>
2017-04-06 06:30:08 +0700

    Calendar: init

---
+++ Belyi.Nsudotnet.Calendar/Belyi.Nsudotnet.Calendar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Belyi.Nsudotnet.Calendar
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
